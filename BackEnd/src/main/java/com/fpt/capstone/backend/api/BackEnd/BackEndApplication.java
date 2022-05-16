6908fbfca62061392c59122a07ad202f3cf07508
hosyhaiha <64488244+hosyhaiha@users.noreply.github.com>
2022-05-16 23:52:39 +0700

    Commit form code

---
+++ BackEnd/src/main/java/com/fpt/capstone/backend/api/BackEnd/BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.capstone.backend.api.BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+}
